0a9052c03fd0459f96c0372344e6879e4ec673b5
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-02-28 17:40:07 -0300

    afterPriorityFix

--- src/main/java/calculadoraLogica/Operations.java
+++ src/main/java/calculadoraLogica/Operations.java
@@ -88,12 +88,31 @@ public class Operations {
     }
     
     public boolean hasHigherPrecedence(char connective1, char connective2) {
-        if (connective2 == '(' || connective2 == ')') {
-            return false;
-        } else if ((connective1 == '∧' && connective2 == 'v') || (connective1 == connective2)) {
-            return false;
-        } else {
+        int priority1 = 0;
+        int priority2 = 0;
+
+        for(char ch = connective1, i = 0;  i<2 ;ch = connective2, i++) {
+            switch (ch) {
+                case '∧':
+                  if (i == 0) priority1 = 1;  
+                  else priority2 = 1;      
+                case 'v':
+                  if (i == 0) priority1 = 2;  
+                  else priority2 = 2;    
+                case '→':
+                  if (i == 0) priority1 = 3;  
+                  else priority2 = 3;       
+                case '↔':
+                  if (i == 0) priority1 = 4;  
+                  else priority2 = 4;     
+            } 
+        }
+        
+        if(priority1>priority2) {
             return true;
+        }
+        else {
+            return false;
         }
     }
     
@@ -106,7 +125,7 @@ public class Operations {
         }
         
         else if(connective == '∧') {
-            if(operand1 == true && operand2 == false) {
+            if(operand1 == true && operand2 == true) {
                 return true;
             }
             else return false;
@@ -119,7 +138,6 @@ public class Operations {
             else return false;
         }
 
-        //else if(node.getConnector() == '↔') {
         else {
             if((operand1 == true && operand2 == true) || (operand1 == false && operand2 == false)) {
                 return true;
@@ -127,127 +145,4 @@ public class Operations {
             else return false;
         }
     }
-    /*public Node GenerateNode(String expression) {
-        Node node = new Node();
-
-        for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
-
-            if(ch != 'P' && ch != 'Q' && ch != 'R' && ch != '(' && ch != ')') {
-                node.setConnector(ch);
-            } else if (node.getConnector() == 'a') {
-                node.setLeft(node.getLeft() + ch);
-            } else {
-                node.setRight(node.getRight() + ch);
-            }
-        }
-
-        return node;
-    }
-    
-    public String GenerateResult(Node node, char left, char right) {
-        if(node.getConnector() == 'v') {
-            if(left == 'V' || right == 'V') {
-                return "Verdadeiro";
-            }
-            else return "Falso";
-        }
-
-        else if(node.getConnector() == '∧') {
-            if(left == 'V' && right == 'V') {
-                return "Verdadeiro";
-            }
-            else return "Falso";
-        }
-
-        else if(node.getConnector() == '→') {
-            if((left == 'V' && right == 'V') || (left == 'F' && right == 'V') || (left == 'F' && right == 'F')) {
-                return "Verdadeiro";
-            }
-            else return "Falso";
-        }
-
-        //else if(node.getConnector() == '↔') {
-        else {
-            if((left == 'V' && right == 'V') || (left == 'F' && right == 'F')) {
-                return "Verdadeiro";
-            }
-            else return "Falso";
-        }
-    }
-    
-    public TreeNode createTree(String formula) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode root = null;
-        boolean isNegated = false;
-        
-        for (int i = 0; i < formula.length(); i++) {
-            char ch = formula.charAt(i);
-            
-            if (ch == '(') {
-                stack.push(root);
-                root = null;
-                isNegated = false;
-            } else if (ch == ')') {
-                TreeNode node = stack.pop();
-                
-                if (root == null) {
-                    root = node;
-                } else if (node.label.equals("∧")) {
-                    node.left = root;
-                    node.right = stack.pop();
-                    root = node;
-                } else if (node.label.equals("∨")) {
-                    node.left = root;
-                    node.right = stack.pop();
-                    root = node;
-                } else if (node.label.equals("→")) {
-                    node.right = root;
-                    node.left = stack.pop();
-                    root = node;
-                } else if (node.label.equals("↔")) {
-                    node.left = root;
-                    node.right = stack.pop();
-                    root = node;
-                }
-            } else if (ch == '-') {
-                isNegated = true;
-            } else if (ch == '¬') {
-                isNegated = true;
-            } else if (ch == '∧') {
-                root = new TreeNode("∧", false);
-            } else if (ch == '∨') {
-                root = new TreeNode("∨", false);
-            } else if (ch == '→') {
-                root = new TreeNode("→", false);
-            } else if (ch == '↔') {
-                root = new TreeNode("↔", false);
-            } else {
-                root = new TreeNode(Character.toString(ch), isNegated);
-                isNegated = false;
-            }
-        }
-        
-        return root;
-    }
-    
-    public boolean tableaux(TreeNode node) {
-        if (node == null) {
-            return true;
-        } else if (node.isNegated) {
-            return !tableaux(node.left);
-        } else if (node.label.equals("∧")) {
-            return tableaux(node.left) && tableaux(node.right);
-        } else if (node.label.equals("∨")) {
-            return tableaux(node.left) || tableaux(node.right);
-        } else if (node.label.equals("→")) {
-            return !tableaux(node.left) || tableaux(node.right);
-        } else if (node.label.equals("↔")) {
-            return tableaux(node.left) == tableaux(node.right);
-        } else {
-            return false;
-        }
-    }*/
 }
-
-
